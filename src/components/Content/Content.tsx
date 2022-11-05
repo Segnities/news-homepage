82f280f69879f4874804225f62a80ce38e088608
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2022-11-05 10:43:48 +0300

    news-content-wrapper almost done

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -24,22 +24,31 @@ const Content = () => {
             />
           </picture>
           <section className={styles["news-intro"]}>
-            <h1>The Bright Future Of Web 3.0</h1>
-            <div>
+            <h1>The Bright Future Of Web 3.0?</h1>
+            <div className={styles["news-intro-description"]}>
               <p>
                 We dive into the next evoulution of the web that claims to put
                 the power of the platforms back into the hands of the people.
                 But is it really fulfilling its promise?
               </p>
-              <button>READ MORE</button>
+              <button>
+                <a href="https://www.oxfordstudent.com/2020/05/08/why-spongebob-squarepants-is-quite-possibly-the-best-cartoon-ever/">
+                  READ MORE
+                </a>
+              </button>
             </div>
           </section>
         </article>
         <article className={styles["new-news"]}>
-            <h1>New</h1>
-            <section>
-                <h2>Hydrogen VS Electrics cars</h2>
-            </section>
+          <h2>New</h2>
+          <section className={styles['new-news-item']}>
+            <h3>
+              <a href="https://www.oxfordstudent.com/2020/05/08/why-spongebob-squarepants-is-quite-possibly-the-best-cartoon-ever/">
+                Hydrogen VS Electrics cars
+              </a>
+            </h3>
+            <p>Will hydrogen-fueled cars ever catch up to EVs</p>
+          </section>
         </article>
       </div>
       <article className={styles["top-news"]}></article>
